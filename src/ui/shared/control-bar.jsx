1fba48d9e7a3d07b388307b7d3c96b0582a1efe9
Ethan Rutherford <ethanrutherford2@gmail.com>
2022-01-09 21:54:09 -0800

    Add back and new buttons

--- src/ui/shared/control-bar.jsx
+++ src/ui/shared/control-bar.jsx
@@ -1,13 +1,23 @@
+import Back from "../../../images/back";
+import Plus from "../../../images/plus";
 import Undo from "../../../images/undo";
 import Redo from "../../../images/redo";
 import styles from "./control-bar.css";
+import {useRouter} from "./app-router";
 
-export function ControlBar({undo, redo, children}) {
+export function ControlBar({newGame, undo, redo}) {
+	const {home} = useRouter();
+
 	return (
 		<div className={styles.controlBar}>
 			<div className={styles.content}>
-				<div>
-					{children}
+				<div className={styles.buttons}>
+					<button className={styles.button} onClick={home}>
+						<Back />
+					</button>
+					<button className={styles.button} onClick={newGame}>
+						<Plus />
+					</button>
 				</div>
 				<div className={styles.buttons}>
 					<button className={styles.button} onClick={undo}>
