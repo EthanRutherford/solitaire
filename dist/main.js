bbfc033d610d8614fdfa5bc010f5e41c84e26606
EthanRutherford <ethanrutherford2@gmail.com>
2022-12-04 20:30:32 +0000

    Deploying to gh-pages from @ EthanRutherford/solitaire@998259250fc5c0a9a632bd92ad9a138d143f6451 🚀

--- dist/main.js
+++ dist/main.js
@@ -3489,10 +3489,10 @@ function Menu() {
       children: "add to homescreen"
     }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("a", {
       className: _menu_css__WEBPACK_IMPORTED_MODULE_2__["default"].version,
-      href: "https://github.com/EthanRutherford/solitaire/projects/1",
+      href: "https://github.com/users/EthanRutherford/projects/1",
       target: "_blank",
       rel: "noreferrer",
-      children: "v0.3"
+      children: "v0.4"
     })]
   });
 }
